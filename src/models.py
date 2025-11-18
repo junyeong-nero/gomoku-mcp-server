4cd8d638935f14c91018b543d2cb8476f158f564
junyeong-nero <magicwho@unist.ac.kr>
2025-11-18 14:15:42 +0900

    [fix] update models

--- src/models.py
+++ src/models.py
@@ -4,5 +4,6 @@ AVAILABLE_MODELS = [
     {"id": "openai/gpt-5.1", "name": "GPT-5.1"},
     {"id": "anthropic/claude-sonnet-4.5", "name": "Claude Soneet 4.5"},
     {"id": "x-ai/grok-4-fast", "name": "Grok4 Fast"},
-    {"id": "tngtech/deepseek-r1t2-chimera:free", "name": "deepseek-r1t2-chimera"},
+    {"id": "moonshotai/kimi-k2-thinking", "name": "kimi-k2-thinking"},
+    {"id": "moonshotai/kimi-k2-0905", "name": "kimi-k2-0905"},
 ]
